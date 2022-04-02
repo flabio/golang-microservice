89bb99aa587569393c01ebf0e0fb3bdecce1de9c
flabiohs <flabiohs@gmail.com>
2022-04-02 02:21:13 -0500

    db

--- database/db.sql
+++ database/db.sql
@@ -1,34 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2022 at 09:20 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
-use banking;
-create table customers(
-    customer_id int(11)not null auto_increment,
-    name varchar(100)not null,
-    date_of_birth date not null,
-    city varchar(100) not null,
-    zipcode varchar(10)not null,
-    status tinyint(4)not null default "1",    primary key(customer_id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table accounts(
-    account_id int(11) not null auto_increment,
-    customer_id int(11) not null,
-    openling_date datetime not null default current_timestamp,
-    account_type varchar(10) not null,
-    pin varchar(10) not null,
-    status tinyint(4) not null default 1,
-    primary key (account_id),
-  
-   foreign key (customer_id) references customers(customer_id)
-);
 
-create table transactions(
-    transaction_id int(11) not null auto_increment,
-    account_id int(11) not null,
-    amount int(11) not null,
-    transaction_type varchar(10) not null,
-    transaction_date datetime not null default current_timestamp,
-    primary key(transaction_id),
-  
-    foreign key (account_id) references accounts(account_id)
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `banking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `account_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `openling_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `account_type` varchar(10) NOT NULL,
+  `pin` varchar(10) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `zipcode` varchar(10) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `name`, `date_of_birth`, `city`, `zipcode`, `status`) VALUES
+(1, 'Golang', '2022-12-12', 'Bogota', '1111', 1),
+(2, 'Python', '2021-12-12', 'Buenaventura', '1221', 1),
+(3, 'React', '2020-10-12', 'Medellin', '2121', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `transaction_type` varchar(10) NOT NULL,
+  `transaction_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`account_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
